bf6abfe08b85a576a8875ddf130a07ef69758b18
Chunky <jasonc@nulogy.com>
2020-05-01 09:52:24 -0400

    refactor

--- javascript/TennisGame3.js
+++ javascript/TennisGame3.js
@@ -1,33 +1,41 @@
-export var TennisGame3 = function(p1N, p2N) {
-    this.p2 = 0;
+export class TennisGame3 {
+  constructor(name1, name2) {
+    this.name1 = name1;
+    this.name2 = name2;
     this.p1 = 0;
+    this.p2 = 0;
+  }
 
-    this.p1N = p1N;
-    this.p2N = p2N;
-};
-
-TennisGame3.prototype.getScore = function() {
-    var s;
-    if ((this.p1 < 4 && this.p2 < 4) && (this.p1 + this.p2 < 6)) {
-        var p = ["Love", "Fifteen", "Thirty", "Forty"];
-        s = p[this.p1];
-        return (this.p1 == this.p2) ? s + "-All" : s + "-" + p[this.p2];
-    } else {
-        if (this.p1 == this.p2)
-            return "Deuce";
-        s = this.p1 > this.p2 ? this.p1N : this.p2N;
-        return ((this.p1 - this.p2) * (this.p1 - this.p2) == 1) ? "Advantage " + s : "Win for " + s;
+  getScore() {
+    if (this.p1 == this.p2 && this.p1 <= 2) {
+      return `${this.description(this.p1)}-All`;
     }
-};
+    if (this.p1 == this.p2) {
+      return "Deuce";
+    }
+    if (Math.max(this.p1, this.p2) <= 3) {
+      return `${this.description(this.p1)}-${this.description(this.p2)}`;
+    }
+    if (Math.abs(this.p1 - this.p2) === 1) {
+      return `Advantage ${this.leader()}`;
+    }
+    return `Win for ${this.leader()}`;
+  }
 
-TennisGame3.prototype.wonPoint = function(playerName) {
-    if (playerName == "player1")
-        this.p1 += 1;
-    else
-        this.p2 += 1;
+  description(score) {
+    return {
+      "0": "Love",
+      "1": "Fifteen",
+      "2": "Thirty",
+      "3": "Forty"
+    }[score];
+  }
 
-};
+  leader() {
+    return this.p1 > this.p2 ? this.name1 : this.name2;
+  }
 
-if (typeof window === "undefined") {
-    module.exports = TennisGame3;
+  wonPoint(name) {
+    name === this.name1 ? (this.p1 += 1) : (this.p2 += 1);
+  }
 }
