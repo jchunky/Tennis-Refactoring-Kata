982122fd01e578d342f79fe44beede07cd588bf2
Chunky <jasonc@nulogy.com>
2020-05-02 01:30:31 -0400

    refactor

--- javascript/TennisGame2.js
+++ javascript/TennisGame2.js
@@ -1,3 +1,32 @@
+function calculateScore(name1, name2, p1, p2) {
+  if (p1 === p2 && p1 <= 2) {
+    return `${description(p1)}-All`;
+  }
+  if (p1 === p2) {
+    return "Deuce";
+  }
+  if (Math.max(p1, p2) <= 3) {
+    return `${description(p1)}-${description(p2)}`;
+  }
+  if (Math.abs(p1 - p2) === 1) {
+    return `Advantage ${leader()}`;
+  }
+  return `Win for ${leader()}`;
+
+  function description(points) {
+    return {
+      "0": "Love",
+      "1": "Fifteen",
+      "2": "Thirty",
+      "3": "Forty"
+    }[points];
+  }
+
+  function leader() {
+    return p1 > p2 ? name1 : name2;
+  }
+}
+
 export class TennisGame2 {
   constructor(name1, name2) {
     this.name1 = name1;
@@ -6,36 +35,11 @@ export class TennisGame2 {
     this.p2 = 0;
   }
 
-  getScore() {
-    if (this.p1 === this.p2 && this.p1 <= 2) {
-      return `${this.description(this.p1)}-All`;
-    }
-    if (this.p1 === this.p2) {
-      return "Deuce";
-    }
-    if (Math.max(this.p1, this.p2) <= 3) {
-      return `${this.description(this.p1)}-${this.description(this.p2)}`;
-    }
-    if (Math.abs(this.p1 - this.p2) === 1) {
-      return `Advantage ${this.leader()}`;
-    }
-    return `Win for ${this.leader()}`;
-  }
-
   wonPoint(name) {
     name === this.name1 ? this.p1++ : this.p2++;
   }
 
-  description(score) {
-    return {
-      "0": "Love",
-      "1": "Fifteen",
-      "2": "Thirty",
-      "3": "Forty"
-    }[score];
-  }
-
-  leader() {
-    return this.p1 > this.p2 ? this.name1 : this.name2;
+  getScore() {
+    return calculateScore(this.name1, this.name2, this.p1, this.p2);
   }
 }
